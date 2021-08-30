373c02f750b5c40c2f44f60b43b39bc353c8b48e
Terje Espedal <terje.m.espedal@gmail.com>
2021-08-30 11:52:10 +0200

    Add new CamelCase to snake_case function and parse all dataframe columns through it.

--- gamepass_fetcher.py
+++ gamepass_fetcher.py
@@ -1,14 +1,12 @@
+import re
 from typing import List, Dict, Optional
 
 import arrow
 import numpy as np
 import pandas as pd
 import requests
-from sqlalchemy import create_engine
 
 from opencritic_fethcer import search_game, get_game_reviews
-
-eng = create_engine('sqlite:///:gamepass:')
 
 # URLs used for getting gamepass json data
 CATALOG_URL = "https://catalog.gamepass.com/sigls/v2?id=29a81209-df6f-41fd-a528-2ae6b91f719c&language=en-us&market=US"
@@ -64,7 +62,7 @@ class GamePass:
         # Use to_pandas method to flatten json and clean data
         df = self.to_pandas_df(json_data)
         # Set game title as index and use pandas to dict to return a dict with name as key and info as values
-        df.set_index("ProductTitle", inplace=True)
+        df.set_index("product_title", inplace=True)
         return df.to_dict(orient="index")
 
     def get_opencritic_reviews(self):
@@ -73,18 +71,19 @@ class GamePass:
         :return Dict: Dict with game name as key and review scores as value
         """
         for game, info in self.catalogue.items():
-            short_title = info["short_title"]
+            # short_title = info["short_title"]
             opencritic_id = search_game(game)
 
             if type(opencritic_id) == int:
                 reviews = get_game_reviews(opencritic_id)
+                reviews = {GamePass.to_snake_case(k): v for k, v in reviews.items()}
                 self.catalogue[game]["opencritic"] = reviews
             else:
                 self.catalogue[game]["opencritic"] = None
 
     def top_n_reviewed(self, n: Optional[int] = 10) -> pd.DataFrame:
         """
-        Returns n revies sorted by review score from high to low, 100 being highest.
+        Returns n reviews sorted by review score from high to low, 100 being highest.
         :param n: Number of wanted results
         :return pd.DataFrame: A pandas dataframe
         """
@@ -94,6 +93,10 @@ class GamePass:
         reviews_df = reviews_df.dropna(subset=["opencritic"])
         reviews_df["average_score"] = reviews_df["opencritic"].apply(lambda x: round(x.get("averageScore"), 2))
         return reviews_df.nlargest(n, "average_score")
+
+    @staticmethod
+    def to_snake_case(text: str) -> str:
+        return re.sub(r'(?<!^)(?=[A-Z])', '_', text).lower()
 
     @staticmethod
     def to_pandas_df(data):
@@ -110,26 +113,31 @@ class GamePass:
         # Use json_normalize to flatten json
         df = pd.json_normalize(data, "LocalizedProperties", ["LastModifiedDate", "MarketProperties"])
         df = df.drop(columns=columns_to_drop)
+
+        # Change column names from camelcase to snake
+        df.columns = [GamePass.to_snake_case(column) for column in df.columns]
+
         # Replace empty strings with NaN to since necessary to use fillna
         df = df.replace(r'^\s*$', np.nan, regex=True)
 
         # Fill "ShorTitle" with values from "SortTitle" where missing and "SortTitle" has values.
-        df["ShortTitle"] = df["ShortTitle"].fillna(df["SortTitle"])
+        df["short__title"] = df["short_title"].fillna(df["sort_title"])
         # Get average rating and n_ratings all time from MarketProperties which also contains 7 and 30 days
-        df["user_rating"] = df["MarketProperties"].apply(lambda x: x["UsageData"][-1]["AverageRating"]).astype("int")
-        df["n_user_rating"] = df["MarketProperties"].apply(lambda x: x["UsageData"][-1]["RatingCount"]).round()
+        df["user_rating"] = df["market_properties"].apply(lambda x: x["UsageData"][-1]["AverageRating"]).astype("int")
+        df["n_user_rating"] = df["market_properties"].apply(lambda x: x["UsageData"][-1]["RatingCount"]).round()
         # Get URL for poster from Images for use in visualization.
-        df["poster_url"] = df["Images"].apply(lambda x: x[4]["Uri"])
+        df["poster_url"] = df["images"].apply(lambda x: x[4]["Uri"])
         # Drop columns that have been processed and are no longer needed
-        df = df.drop(columns=["Images", "MarketProperties", "SortTitle"], axis=1)
+        df = df.drop(columns=["images", "market_properties", "sort_title", "short_title"], axis=1)
         # Move "ShorTitle" first in dataframe and drop duplicate middle column
-        df.insert(0, "short_title", df['ShortTitle'])
-        df = df.drop("ShortTitle", axis=1)
+        df.insert(0, "short_title", df['short__title'])
+        df = df.drop("short__title", axis=1)
 
         return df
 
 
 if __name__ == '__main__':
     xpass = GamePass()
-    xpass.top_n_reviewed()
+    reviews = xpass.top_n_reviewed()
+    print(reviews)
 # df = xpass.to_pandas_df()
